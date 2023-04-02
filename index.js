56873349424bd7062b169f8bd6f9e1da038bf4b2
Husen <saidinahusen10.hs@gmail.com>
2023-04-02 21:07:59 +0700

    p

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ console.log("Data Users :\n")
 console.log(users)
 console.log("\n============================================\n")
 
-// console.log("no 1 bang (Temukan orang yang memiliki banyak keterampilan di objek users)")
-// answer1() // 1. Temukan orang yang memiliki banyak keterampilan di objek users
-// console.log("\nno 2 bang (Hitung pengguna yang login, hitung pengguna yang memiliki lebih dari dan sama dengan 50 poin dari objek users)")
-// answer2() // 2. Hitung pengguna yang login, hitung pengguna yang memiliki lebih dari dan sama dengan 50 poin dari objek users
+console.log("no 1 bang (Temukan orang yang memiliki banyak keterampilan di objek users)")
+answer1() // 1. Temukan orang yang memiliki banyak keterampilan di objek users
+console.log("\nno 2 bang (Hitung pengguna yang login, hitung pengguna yang memiliki lebih dari dan sama dengan 50 poin dari objek users)")
+answer2() // 2. Hitung pengguna yang login, hitung pengguna yang memiliki lebih dari dan sama dengan 50 poin dari objek users
 console.log("\nno 3 bang (Temukan orang yang merupakan developer stack MERN (Mongo, Express, React, Node) dari objek users)")
 answer3() // 3. Temukan orang yang merupakan developer stack MERN (Mongo, Express, React, Node) dari objek users
-// console.log("\nno 4 bang (Masukan nama Anda di objek users tanpa mengubah objek users asli)")
-// answer4() // 4. Masukan nama Anda di objek users tanpa mengubah objek users asli
-// // console.log("\nno 5 bang (Dapatkan semua kunci atau properti objek user)")
-// answer5() // 5. Dapatkan semua kunci atau properti objek user
-// console.log("\nno 6 bang (Dapatkan semua point objek user)")
-// answer6() // 6. Dapatkan semua point objek user
-// console.log()
+console.log("\nno 4 bang (Masukan nama Anda di objek users tanpa mengubah objek users asli)")
+answer4() // 4. Masukan nama Anda di objek users tanpa mengubah objek users asli
+console.log("\nno 5 bang (Dapatkan semua kunci atau properti objek user)")
+answer5() // 5. Dapatkan semua kunci atau properti objek user
+console.log("\nno 6 bang (Dapatkan semua point objek user)")
+answer6() // 6. Dapatkan semua point objek user
+console.log()
